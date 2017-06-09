26d4d7d38b114367198e76fe3c28410085df60e2
U-UTICORP\jgardner <jgardner@U90-STFL-44.uticorp.com>
2017-06-08 17:21:13 -0700

    deleted unnecessary javascript file

--- js/css.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  // TODO: define css
-  var css = 'body { width: 200px !important; }';
-
-  var style       = document.createElement('style');
-  style.innerHTML = css;
-
-  document.head.appendChild(style);
-})();
