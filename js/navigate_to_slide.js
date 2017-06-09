7db8594b28ca08e5a5f7eb450ecb205b726a3edf
Mitchell Sotto <msotto@uticorp.com>
2017-06-09 08:53:50 -0700

    added navigate_to_slide.js

--- js/navigate_to_slide.js
+++ js/navigate_to_slide.js
@@ -1,7 +1,24 @@
 (function() {
 
-	var slide_reference = prompt("Please enter the 3 number sequence of the slide you want to go to (last three numbers)", "1-01-1"); 
-	loadSlideByReferenceId(slide_reference); 
+	var navigate_slides = function(){
 
-})()
+		this.select_slide = function(){
+			
+			var slide_reference = prompt("Please enter the 3 number sequence of the slide you want to go to (last three numbers)", "1-01-1"); 
+			loadSlideByReferenceId(slide_reference); 
+
+		};
+
+		//takes you to the next slide
+		this.next_slide = function(){
+			loadNextSlide();
+		};
+
+		//takes you to the previous slide
+		this.prev_slide = function(){
+			loadPrevSlide();
+		};
+	}
+
+})();
 
